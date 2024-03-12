6d7e31a7759114b033d8271fe6beedfe2176c0bb
ShaneVCP <shane.hewitt@veraleo.co>
2024-03-12 16:04:30 -0400

    add hotjar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,14 @@ import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import Navbar from '@/components/navbar';
 import '../styles/globals.css';
+import { hotjar } from 'react-hotjar';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps: { session, ...pageProps} }: AppProps) {
+  useEffect(() => {
+    hotjar.initialize(3902622, 6);
+  }, [])
+
   return (
     <SessionProvider session={session}>
         <Navbar />
